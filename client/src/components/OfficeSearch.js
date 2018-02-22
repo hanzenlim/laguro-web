c41a6c60a613b1da42ef3293470cbf3fc71b34e5
currymich <currymich@gmail.com>
2018-02-22 12:28:55 -0800

    connecting actions to search to enable form to pass values

--- client/src/components/OfficeSearch.js
+++ client/src/components/OfficeSearch.js
@@ -1,82 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import * as actions from "../actions";
 
-import 'react-datepicker/dist/react-datepicker.css';
-
+import "react-datepicker/dist/react-datepicker.css";
 
 class OfficeSearch extends Component {
+	onSubmit(values) {
+		const { reset } = this.props;
 
-  onSubmit(values) {
-    const {reset} = this.props;
+		this.props.searchOffices(values);
+		reset();
+	}
 
-    console.log(values);
-    reset();
-  }
+	renderField = ({ input, label, placeholder, meta: { touched, error } }) => (
+		<div>
+			<label>{label}</label>
+			<div>
+				<input {...input} placeholder={placeholder} />
+			</div>
+			{touched && error && <span>{error}</span>}
+		</div>
+	);
 
-  renderField = ({input, label, placeholder, meta: {touched, error} }) => (
-    <div>
-      <label>{label}</label>
-      <div>
-        <input {...input} placeholder={placeholder} />
-      </div>
-      {touched && error && <span>{error}</span>}
-    </div>
-  );
+	renderDatePicker = ({ input, label, meta: { touched, error } }) => (
+		<div>
+			<label>{label}</label>
+			<DatePicker
+				{...input}
+				selected={input.value ? moment(input.value, "MMM DD, YYYY") : null}
+				dateFormat="MMM DD, YYYY"
+				placeholderText={moment().format("MMM DD, YYYY")}
+			/>
+			{touched && error && <span>{error}</span>}
+		</div>
+	);
 
+	render() {
+		const { handleSubmit, pristine, reset, submitting } = this.props;
 
-  renderDatePicker = ({input, label, meta: {touched, error} }) => (
-    <div>
-      <label>{label}</label>
-      <DatePicker {...input}
-        selected={input.value ? moment(input.value, 'MMM DD, YYYY') : null}
-        dateFormat="MMM DD, YYYY"
-        placeholderText={moment().format("MMM DD, YYYY")}
-      />
-      {touched && error && <span>{error}</span>}
-    </div>
-  );
-
-  render() {
-    const { handleSubmit, pristine, reset, submitting } = this.props;
-
-    return (
-      <form
-        className="searchModule toggle active" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-        <Field
-          name="location"
-          label="Where"
-          placeholder="Office form"
-          component={this.renderField}
-        />
-        <Field
-          name="date"
-          label="When"
-          component={this.renderDatePicker}
-        />
-        <div className="form-buttons">
+		return (
+			<form
+				className="searchModule toggle active"
+				onSubmit={handleSubmit(this.onSubmit.bind(this))}
+			>
+				<Field
+					name="location"
+					label="Where"
+					placeholder="Office form"
+					component={this.renderField}
+				/>
+				<Field name="date" label="When" component={this.renderDatePicker} />
+				<div className="form-buttons">
 					<button
-            className="waves-effect btn green lighten-2"
-            type="submit"
-            disabled={pristine || submitting}>
+						className="waves-effect btn green lighten-2"
+						type="submit"
+						disabled={pristine || submitting}
+					>
 						Submit
 					</button>
-          <button
-            className="waves-effect btn red lighten-2"
-            type="button"
-            disabled={pristine || submitting}
-            onClick={reset}>
-            Clear
-          </button>
+					<button
+						className="waves-effect btn red lighten-2"
+						type="button"
+						disabled={pristine || submitting}
+						onClick={reset}
+					>
+						Clear
+					</button>
 				</div>
-      </form>
-    )
-  }
-
+			</form>
+		);
+	}
 }
 
 export default reduxForm({
-  form: "officeSearch"
-})(connect(null)(OfficeSearch));
+	form: "officeSearch"
+})(connect(null, actions)(OfficeSearch));
