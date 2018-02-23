f1e0969b9560451a72662ec41fb3a0e0832f7733
currymich <currymich@gmail.com>
2018-02-22 23:23:50 -0800

    Changing project name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# swapop
+# LAGURO
 Dental Space Rental
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const logger = require('morgan');
 const keys = require("./config/keys");
 
 // DB config
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost/swapop";
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost/laguro";
 
 mongoose.connect(mongoURI, {useMongoClient: true});
 mongoose.Promise = global.Promise;
