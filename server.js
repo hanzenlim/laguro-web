475824dfec585b24a668532c93cc6a907aec2116
currymich <currymich@gmail.com>
2018-04-07 17:26:17 -0700

    Attaching the new routes and the new schema to backend server

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const path = require('path');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-
+const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 // Local Packages
 const keys = require("./client/src/config/keys");
@@ -14,14 +13,15 @@ const keys = require("./client/src/config/keys");
 // DB config
 const mongoURI = process.env.MONGODB_URI || "mongodb://localhost/laguro";
 
-mongoose.connect(mongoURI, {useMongoClient: true});
+mongoose.connect(mongoURI, { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 // Models
 require("./models/User");
-require("./models/Dentist")
-require("./models/Listing")
-require("./models/Office")
+require("./models/Dentist");
+require("./models/Listing");
+require("./models/Office");
+require("./models/Review");
 
 // Services
 require("./services/passport");
@@ -33,34 +33,35 @@ app.use(bodyParser.json());
 
 // Auth config
 app.use(
-	cookieSession({
-		maxAge: 10800000, // 1 day
-		keys: [keys.cookieKey]
-	})
+  cookieSession({
+    maxAge: 10800000, // 1 day
+    keys: [keys.cookieKey]
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route Files
 require("./routes/authRoutes")(app);
 require("./routes/dentistRoutes")(app);
 require("./routes/officeRoutes")(app);
 require("./routes/listingRoutes")(app);
+require("./routes/reviewRoutes")(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // express will serve production assets (main.js/css)
-	app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
   // express will serve index.html if unrec. route
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Server Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log("Server listening on 5000");
+  console.log("Server listening on 5000");
 });
