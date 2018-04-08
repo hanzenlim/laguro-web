9cf86e1f215ace2f5078809b13c2e0fe8176b392
currymich <currymich@gmail.com>
2018-04-07 17:26:33 -0700

    Hooking up the new reducer to store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,18 @@
-import { combineReducers } from 'redux';
-import dentistsReducer from './dentistsReducer';
-import officesReducer from './officesReducer';
-import listingsReducer from './listingsReducer';
-import authReducer from './authReducer';
-import filtersReducer from './filtersReducer';
-import { reducer as reduxFormReducer } from 'redux-form';
+import { combineReducers } from "redux";
+import dentistsReducer from "./dentistsReducer";
+import officesReducer from "./officesReducer";
+import listingsReducer from "./listingsReducer";
+import authReducer from "./authReducer";
+import filtersReducer from "./filtersReducer";
+import reviewsReducer from "./reviewsReducer";
+import { reducer as reduxFormReducer } from "redux-form";
 
 export default combineReducers({
   dentists: dentistsReducer,
   listings: listingsReducer,
   offices: officesReducer,
   filters: filtersReducer,
+  reviews: reviewsReducer,
   auth: authReducer,
   form: reduxFormReducer
 });
