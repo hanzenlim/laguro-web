0e3c82a7c4aff9972a465ea3716549d88cbc74f9
currymich <currymich@gmail.com>
2018-04-11 12:04:08 -0700

    modifying render conditional to prevent undefined array error

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -12,11 +12,11 @@ class OfficeResultIndex extends Component {
 
     this.props.getOneListing(this.listing_id);
     this.props.getOneOffice(this.office_id);
-		this.props.fetchReviews(this.office_id)
+    this.props.fetchReviews(this.office_id);
   }
 
   renderEquipment(listing) {
-    if (listing.equipment.length) {
+    if (listing.equipment && listing.equipment.length) {
       return listing.equipment.map(equipment => (
         <li className="listingRow" key={equipment.name}>
           {equipment.name} - ${equipment.price}
@@ -28,7 +28,7 @@ class OfficeResultIndex extends Component {
   }
 
   renderStaff(listing) {
-    if (listing.staff.length) {
+    if (listing.staff && listing.staff.length) {
       return listing.staff.map(staff => (
         <li className="listingRow" key={staff.role}>
           {staff.role} - ${staff.price} ({staff.count} available)
@@ -57,81 +57,81 @@ class OfficeResultIndex extends Component {
       return <div>Loading...</div>;
     }
 
-		// calculate avg rating
-		if (reviews && reviews.length) {
-			this.avg_rating =
-				reviews.map(review => (review.rating)).reduce((acc, rating) => acc + rating) / reviews.length;
-			this.rating_count = reviews.length;
-		} else {
-			this.avg_rating = 0;
-			this.rating_count = 0;
-		}
+    // calculate avg rating
+    if (reviews && reviews.length) {
+      this.avg_rating =
+        reviews
+          .map(review => review.rating)
+          .reduce((acc, rating) => acc + rating) / reviews.length;
+      this.rating_count = reviews.length;
+    } else {
+      this.avg_rating = 0;
+      this.rating_count = 0;
+    }
 
-    if (office && listing) {
-      return (
-        <div className="listing">
-          <div className="officeImgs">{this.renderImages(office)}</div>
+    return (
+      <div className="listing">
+        <div className="officeImgs">{this.renderImages(office)}</div>
 
-          <div className="listingDetails">
-            <div className="listingHeader">
-              <div>
-                <h3>{office.name}</h3>
-                <h5>{office.location}</h5>
-                <blockquote>
-                  <h6>
-                    Rental Window:{" "}
-                    {moment(listing.time_available).format("MMM D, h a")} -
-                    {moment(listing.time_closed).format("h a")}
-                  </h6>
-                </blockquote>
-              </div>
-              <Link
-                className="btn light-blue lighten-2 waves-effect"
-                to={"/offices/search"}
-              >
-                Go back to listings
-              </Link>
-            </div>
-
-            <div className="availableRow">
-              <div>
-                <h5>Staff Available</h5>
-                {this.renderStaff(listing)}
-              </div>
-              <div>
-                <h5>Equipment Available</h5>
-                {this.renderEquipment(listing)}
-              </div>
-            </div>
-
+        <div className="listingDetails">
+          <div className="listingHeader">
             <div>
-              <h6>Cleaning Fee - ${listing.cleaning_fee}</h6>
-              <h6>Rental Charge - 15%</h6>
+              <h3>{office.name}</h3>
+              <h5>{office.location}</h5>
+              <blockquote>
+                <h6>
+                  Rental Window:{" "}
+                  {moment(listing.time_available).format("MMM D, h a - ")}
+                  {moment(listing.time_closed).format("h a")}
+                </h6>
+              </blockquote>
+            </div>
+            <Link
+              className="btn light-blue lighten-2 waves-effect"
+              to={"/offices/search"}
+            >
+              Go back to listings
+            </Link>
+          </div>
+
+          <div className="availableRow">
+            <div>
+              <h5>Staff Available</h5>
+              {this.renderStaff(listing)}
+            </div>
+            <div>
+              <h5>Equipment Available</h5>
+              {this.renderEquipment(listing)}
             </div>
           </div>
 
-          <div className="bookNow">
-            <div className="content">
-              <div>
-                <p>
-                  ${listing.price} <small>hourly per chair</small>
-                </p>
-                <div className="rating">
-                  <ReactStars count={5} value={this.avg_rating} size={10} />
-                  <small>{`(${this.rating_count})`}</small>
-                </div>
-              </div>
-              <Link
-                className="btn red lighten-2 waves-effect"
-                to={"/offices/search"}
-              >
-                Book Now
-              </Link>
-            </div>
+          <div>
+            <h6>Cleaning Fee - ${listing.cleaning_fee}</h6>
+            <h6>Rental Charge - 15%</h6>
           </div>
         </div>
-      );
-    }
+
+        <div className="bookNow">
+          <div className="content">
+            <div>
+              <p>
+                ${listing.price} <small>hourly per chair</small>
+              </p>
+              <div className="rating">
+                <ReactStars count={5} value={this.avg_rating} size={10} />
+                <small>{`(${this.rating_count})`}</small>
+              </div>
+            </div>
+            <Link
+              className="btn red lighten-2 waves-effect"
+              to={"/offices/search"}
+            >
+              Book Now
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -139,7 +139,7 @@ function mapStateToProps(state) {
   return {
     listing: state.listings.selected,
     office: state.offices.selected,
-		reviews: state.reviews.selected,
+    reviews: state.reviews.selected,
     filters: state.filters
   };
 }
