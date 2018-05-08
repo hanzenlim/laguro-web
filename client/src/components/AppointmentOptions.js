fdeafed5466e05054543decd76a830e5349a3458
currymich <currymich@gmail.com>
2018-05-08 13:32:48 -0700

    AUTH: When non-user attempts to book appt, redirects them to login page
    APPTS: Booking an appt also books out adj slots up to procedure duration
    
    MISC: Adding styling, chainging component names, running eslint
    
    DENTIST: Adding default time to edit form so new procedures have duration

---
+++ client/src/components/AppointmentOptions.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import moment from "moment";
+
+class AppointmentOptions extends Component {
+  reserveAppt(procedure) {
+    const { auth, reservation, appointments, index } = this.props;
+    if (auth && auth.data) {
+      for (var i = index; i < appointments.length; i++) {
+        let overlap = this.timeComparison(
+          appointments[index].time,
+          appointments[i].time,
+          procedure.duration
+        );
+
+        if (overlap) {
+          this.props.reserveAppointment(
+            reservation._id,
+            appointments[i]._id,
+            auth.data._id
+          );
+        }
+      }
+    } else {
+      window.location.href = "/auth/google";
+    }
+  }
+
+  //compare two times to see if an appointment at time1 with duration = duration would overlap time2
+  timeComparison(time1, time2, duration) {
+    return moment(time1)
+      .add(duration, "minutes")
+      .isAfter(moment(time2));
+  }
+
+  // Insure that there is enough time for the appointment the patient is attempting to book
+  invalidDurationCheck(reservation, procedure, appointments, index) {
+    // dont allow reservations to go past the closing time
+    let pastClosing = this.timeComparison(
+      appointments[index].time,
+      reservation.time_end,
+      procedure.duration
+    );
+
+    if (pastClosing) {
+      return true;
+    }
+
+    // dont allow reservations that go over into the next time slot if that slot is already reserved
+    for (var i = index; i < appointments.length; i++) {
+      let overlap = this.timeComparison(
+        appointments[index].time,
+        appointments[i].time,
+        procedure.duration
+      );
+
+      let reserved = appointments[i].patient_id;
+
+      if (overlap && reserved) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  renderProcedureButtons() {
+    const { procedures, appointments, index, reservation } = this.props;
+    return procedures.map(procedure => (
+      <button
+        key={procedure.name}
+        className="procedure waves-effect btn light-blue lighten-2"
+        type="button"
+        disabled={this.invalidDurationCheck(
+          reservation,
+          procedure,
+          appointments,
+          index
+        )}
+        onClick={this.reserveAppt.bind(this, procedure)}
+      >
+        {`${procedure.name} - ${procedure.duration} mins`}
+      </button>
+    ));
+  }
+
+  render() {
+    return <div className="procedureBtns">{this.renderProcedureButtons()}</div>;
+  }
+}
+
+export default connect(null, actions)(AppointmentOptions);
--- client/src/components/forms/EditDentist.js
+++ client/src/components/forms/EditDentist.js
@@ -8,86 +8,92 @@ import procedureList from "../../staticData/procedureList";
 import * as actions from "../../actions";
 
 class EditDentist extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			dentist: {},
-			img_url: ""
-		};
-	}
+    this.state = {
+      dentist: {},
+      img_url: ""
+    };
+  }
 
-	componentWillMount() {
-		document.title = "Laguro - Edit Profile";
+  componentWillMount() {
+    document.title = "Laguro - Edit Profile";
 
-		this.getDentist().then(dentist => {
-			this.setState({
-				dentist: dentist,
-				img_url: dentist.img_url
-			});
+    this.getDentist().then(dentist => {
+      this.setState({
+        dentist: dentist,
+        img_url: dentist.img_url
+      });
 
-			this.props.initialize({
-				name: dentist.name,
-				location: dentist.location,
-				specialty: dentist.specialty,
-				procedures: dentist.procedures
-			});
-		});
-	}
+      this.props.initialize({
+        name: dentist.name,
+        location: dentist.location,
+        specialty: dentist.specialty,
+        procedures: dentist.procedures
+      });
+    });
+  }
 
-	onSubmit(values) {
-		const { reset, auth } = this.props;
-		const { img_url } = this.state;
-		this.props.editDentist({ ...values, img_url, id: auth._id });
-		reset();
-	}
+  onSubmit(values) {
+    const { reset, auth } = this.props;
+    const { img_url } = this.state;
+    this.props.editDentist({ ...values, img_url, id: auth._id });
+    reset();
+  }
 
-	// get all dentists and find the dentist profile that matches logged in user
-	async getDentist() {
-		await this.props.fetchDentists();
+  // get all dentists and find the dentist profile that matches logged in user
+  async getDentist() {
+    await this.props.fetchDentists();
 
-		const { dentists, auth } = this.props;
-		if (dentists.length) {
-			const dentist = dentists.filter(dentist => dentist.user_id === auth._id);
-			return dentist[0];
-		} else {
-			return {};
-		}
-	}
+    const { dentists, auth } = this.props;
+    if (dentists.length) {
+      const dentist = dentists.filter(dentist => dentist.user_id === auth._id);
+      return dentist[0];
+    } else {
+      return {};
+    }
+  }
 
-	extractUrlToState(result) {
-		let upload = result.filesUploaded;
-		if (upload.length) {
-			this.setState({ img_url: upload[0].url });
-		}
-	}
+  extractUrlToState(result) {
+    let upload = result.filesUploaded;
+    if (upload.length) {
+      this.setState({ img_url: upload[0].url });
+    }
+  }
 
-	renderUploadedImages() {
-		const { img_url } = this.state;
-		return <img src={img_url} alt="dentist" />;
-	}
+  renderUploadedImages() {
+    const { img_url } = this.state;
+    return <img src={img_url} alt="dentist" />;
+  }
 
-	renderDurations() {
-		return [
-			<option value={30} key={30}>30 minutes</option>,
-			<option value={60} key={60}>60 minutes</option>
-		]
-	}
+  renderDurations() {
+    return [
+      <option value={30} key={30}>
+        30 minutes
+      </option>,
+      <option value={60} key={60}>
+        60 minutes
+      </option>
+    ];
+  }
 
-	renderProcedures() {
-		let procedureOptions = procedureList.map(procedure => {
-			return (
-				<option value={procedure.name} key={procedure.id}>
-					{procedure.name}
-				</option>
-			);
-		});
-		procedureOptions = [
-			<option value="" key={0}>Please select a procedure...</option>,
-			...procedureOptions
-		];
-		return procedureOptions;
-	}
+  renderProcedures() {
+    let procedureOptions = procedureList.map(procedure => {
+      return (
+        <option value={procedure.name} key={procedure.id}>
+          {procedure.name}
+        </option>
+      );
+    });
+    procedureOptions = [
+      <option value="" key={0}>
+        Please select a procedure...
+      </option>,
+      ...procedureOptions
+    ];
+    return procedureOptions;
+  }
 
   renderSelect = ({ input, label, children, meta: { touched, error } }) => {
     return (
@@ -114,7 +120,7 @@ class EditDentist extends Component {
           <Field
             name={`${procedure}.duration`}
             component={this.renderSelect}
-						children={this.renderDurations()}
+            children={this.renderDurations()}
           />
           <button
             type="button"
@@ -126,134 +132,134 @@ class EditDentist extends Component {
           </button>
         </li>
       ))}
-			<li>
-				<button
-					type="button"
-					className="waves-effect btn-flat"
-					onClick={() => fields.push({})}
-					>
-						Add Procedure
-					</button>
-					{error && <span>{error}</span>}
-				</li>
+      <li>
+        <button
+          type="button"
+          className="waves-effect btn-flat"
+          onClick={() => fields.push({ duration: 30 })}
+        >
+          Add Procedure
+        </button>
+        {error && <span>{error}</span>}
+      </li>
     </ul>
   );
 
-	renderField = ({
-		input,
-		label,
-		className,
-		placeholder,
-		meta: { touched, error }
-	}) => (
-		<div className={className}>
-			<label>{label}</label>
-			<div>
-				<input {...input} placeholder={placeholder} />
-			</div>
-			{touched && error && <span className="red-text">{error}</span>}
-		</div>
-	);
+  renderField = ({
+    input,
+    label,
+    className,
+    placeholder,
+    meta: { touched, error }
+  }) => (
+    <div className={className}>
+      <label>{label}</label>
+      <div>
+        <input {...input} placeholder={placeholder} />
+      </div>
+      {touched && error && <span className="red-text">{error}</span>}
+    </div>
+  );
 
-	render() {
-		const { handleSubmit, submitting } = this.props;
+  render() {
+    const { handleSubmit, submitting } = this.props;
 
-		return (
-			<form
-				className="bigForm light-blue lighten-5"
-				onSubmit={handleSubmit(this.onSubmit.bind(this))}
-			>
-				<div className="form_title">
-					<h4>Edit Dentist Profile</h4>
-					<Link
-						className="btn light-blue lighten-2 waves-effect"
-						to={"/profile"}
-					>
-						Go back to profile
-					</Link>
-				</div>
+    return (
+      <form
+        className="bigForm light-blue lighten-5"
+        onSubmit={handleSubmit(this.onSubmit.bind(this))}
+      >
+        <div className="form_title">
+          <h4>Edit Dentist Profile</h4>
+          <Link
+            className="btn light-blue lighten-2 waves-effect"
+            to={"/profile"}
+          >
+            Go back to profile
+          </Link>
+        </div>
 
-				<div className="row">
-					<Field
-						name="name"
-						label="Name"
-						className="col s12 m4"
-						placeholder="Stephen Curry"
-						validate={required}
-						component={this.renderField}
-					/>
-					<Field
-						name="specialty"
-						label="Dental Specialty"
-						className="col s12 m4"
-						placeholder="General Dentist"
-						validate={required}
-						component={this.renderField}
-					/>
-					<Field
-						name="location"
-						label="Location of practice"
-						className="col s12 m4"
-						placeholder="Oakland, CA"
-						validate={required}
-						component={this.renderField}
-					/>
-				</div>
+        <div className="row">
+          <Field
+            name="name"
+            label="Name"
+            className="col s12 m4"
+            placeholder="Stephen Curry"
+            validate={required}
+            component={this.renderField}
+          />
+          <Field
+            name="specialty"
+            label="Dental Specialty"
+            className="col s12 m4"
+            placeholder="General Dentist"
+            validate={required}
+            component={this.renderField}
+          />
+          <Field
+            name="location"
+            label="Location of practice"
+            className="col s12 m4"
+            placeholder="Oakland, CA"
+            validate={required}
+            component={this.renderField}
+          />
+        </div>
 
-				<div className="image_upload">
-					<div className="image_display">{this.renderUploadedImages()}</div>
-					<ReactFilestack
-						apikey={"Aj4gwfCaTS2Am35P0QGrbz"}
-						buttonText="Upload New Image"
-						buttonClass="btn light-blue lighten-2"
-						options={{
-							accept: ["image/*"],
-							imageMin: [300, 300],
-							maxFiles: 1,
-							fromSources: [
-								"local_file_system",
-								"url",
-								"imagesearch",
-								"facebook",
-								"instagram"
-							],
-							storeTo: { container: "office-photos" }
-						}}
-						onSuccess={result => this.extractUrlToState(result)}
-					/>
-				</div>
+        <div className="image_upload">
+          <div className="image_display">{this.renderUploadedImages()}</div>
+          <ReactFilestack
+            apikey={"Aj4gwfCaTS2Am35P0QGrbz"}
+            buttonText="Upload New Image"
+            buttonClass="btn light-blue lighten-2"
+            options={{
+              accept: ["image/*"],
+              imageMin: [300, 300],
+              maxFiles: 1,
+              fromSources: [
+                "local_file_system",
+                "url",
+                "imagesearch",
+                "facebook",
+                "instagram"
+              ],
+              storeTo: { container: "office-photos" }
+            }}
+            onSuccess={result => this.extractUrlToState(result)}
+          />
+        </div>
 
-				<div className="row">
-					<FieldArray
-						name="procedures"
-						className="col s12"
-						component={this.renderProcedureSelector}
-					/>
-				</div>
+        <div className="row">
+          <FieldArray
+            name="procedures"
+            className="col s12"
+            component={this.renderProcedureSelector}
+          />
+        </div>
 
-				<div className="form-buttons">
-					<button
-						className="waves-effect btn light-blue lighten-2"
-						type="submit"
-						disabled={submitting}
-					>
-						Submit
-					</button>
-				</div>
-			</form>
-		);
-	}
+        <div className="form-buttons">
+          <button
+            className="waves-effect btn light-blue lighten-2"
+            type="submit"
+            disabled={submitting}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-const required = value => (value && value !== "" ? undefined : 'Required')
+const required = value => (value && value !== "" ? undefined : "Required");
 
 function mapStateToProps(state) {
-	return {
-		auth: state.auth.data,
-		dentists: state.dentists.dentists
-	};
+  return {
+    auth: state.auth.data,
+    dentists: state.dentists.dentists
+  };
 }
 
 export default reduxForm({
-	form: "editDentist"
+  form: "editDentist"
 })(connect(mapStateToProps, actions)(EditDentist));
