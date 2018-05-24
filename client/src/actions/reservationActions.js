0e13fe2ebc8fd1603eae98e520b7c45196e8eaa7
rrjoson <rr_joson@yahoo.com>
2018-05-24 19:41:19 +0800

    :sparkles: Redirect to success page after create reservation

--- client/src/actions/reservationActions.js
+++ client/src/actions/reservationActions.js
@@ -1,19 +1,21 @@
-import { GET_SELECTED_RESERVATIONS } from './types';
+import { GET_SELECTED_RESERVATIONS, CREATE_RESERVATION } from './types';
 import Reservation from '../models/reservation';
+import history from '../history';
 
 export const createReservation = params => async dispatch => {
-    await Reservation.create(params);
+    const reservation = await Reservation.create(params);
     dispatch({
-        type: GET_SELECTED_RESERVATIONS,
-        payload: null
+        type: CREATE_RESERVATION,
+        payload: reservation,
     });
+    history.push(`/payment-success?reservationId=${reservation.id}`);
 };
 
 export const cancelReservation = reservationId => async dispatch => {
     await Reservation.delete(reservationId);
     dispatch({
         type: GET_SELECTED_RESERVATIONS,
-        payload: null
+        payload: null,
     });
 };
 
@@ -21,6 +23,6 @@ export const queryReservations = (key, value) => async dispatch => {
     const reservations = await Reservation.query(key, value);
     dispatch({
         type: GET_SELECTED_RESERVATIONS,
-        payload: reservations
+        payload: reservations,
     });
 };
