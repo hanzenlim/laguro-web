ba4784675ac85825dc70c17c967e7df9ccbba1cb
Hanzen Lim <hanzen@laguro.com>
2018-06-01 11:37:47 -0700

    Add dev config

---
+++ client/src/config/dev.js
@@ -0,0 +1,15 @@
+// dev.js dev env keys - don't commit this!!!
+module.exports = {
+    // Google OAuth 2.0
+    googleClientID:
+		'358412081949-ch5mfbcol5ko4h7lp8pegb2qrt1ig3m0.apps.googleusercontent.com',
+    googleClientSecret: '3jIFuNKGAuNQ5tuDbgWdm1Yu',
+
+    // filestack api
+    filestackKey: 'AfLcCyAMtTGONprYBDsodz',
+
+    // Cookie-session
+    cookieKey: 'aoiergnpiuarengpiuanrpiughpiuabspevijnwasdjnglij',
+    stripeKey: 'pk_test_N4qTNFY0FVDmA36hKINg7LaP',
+    googleMapApiKey: 'AIzaSyAG734-AbLRsC-h0DfNxcCGWRuSOPK4Hrs',
+};
