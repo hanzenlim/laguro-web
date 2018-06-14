efc04111c66fb3851bc300234945c388f3813f3e
Hanzen Lim <hanzen@laguro.com>
2018-06-14 09:54:08 -0700

    Replace old modal to material-ui modal for login button

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,73 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Flex } from './common';
+import { Modal, Flex } from './common';
 
 import GoogleLoginButton from './images/google-login-button.png';
-import './css/LoginModal.css';
 
 class LoginModal extends Component {
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-    }
-
-    /* Set the wrapper ref */
-    setWrapperRef = node => {
-        this.wrapperRef = node;
-    };
-
-    /* Alert if clicked on outside of element */
-    handleClickOutside = event => {
-        const { onClose } = this.props;
-
-        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-            if (onClose) {
-                onClose();
-            }
-        }
-    };
-
-    handleClose = () => {
-        const { onClose } = this.props;
-
-        if (onClose) {
-            onClose();
-        }
-    };
-
     render() {
         const { open } = this.props;
 
         if (!open) return null;
 
         return (
-            <div>
-                <div ref={this.setWrapperRef} className="modal open">
-                    <a onClick={this.handleClose} className="modal-close">
-                        <i className="material-icons md-38">close</i>
+            <Modal closable open={open} onClose={this.props.onClose}>
+                <Flex justifyContent="center">
+                    <a href="/auth/google">
+                        <img
+                            alt="google login button"
+                            width="300"
+                            height="70"
+                            src={GoogleLoginButton}
+                        />
                     </a>
-                    <div className="modal-header">
-                        <div className="modal-desc">Sign In</div>
-                    </div>
-                    <div className="modal-content">
-                        <Flex justifyContent="center">
-                            <a href="/auth/google">
-                                <img
-                                    alt="google login button"
-                                    width="300"
-                                    height="70"
-                                    src={GoogleLoginButton}
-                                />
-                            </a>
-                        </Flex>
-                    </div>
-                    <div className="modal-footer" />
-                </div>
-                <div className="modal-overlay" />
-            </div>
+                </Flex>
+            </Modal>
         );
     }
 }
