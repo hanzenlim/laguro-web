5d7fe6664fb2d1a4b5c58ed9975dcf83db4dc107
currymich <currymich@gmail.com>
2018-06-15 11:24:02 -0700

    Remove intercom icon

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import styled from "styled-components";
 import logo from './icons/logo.svg';
-import Icon from './Icon';
 
 import './css/Footer.css';
 
 class Footer extends Component {
-
     constructor() {
         super();
         this.state = {
@@ -18,11 +15,11 @@ class Footer extends Component {
     }
 
     componentDidMount() {
-        window.addEventListener("resize", this.updateDimensions);
+        window.addEventListener('resize', this.updateDimensions);
     }
 
     componentWillUnmount() {
-        window.removeEventListener("resize", this.updateDimensions);
+        window.removeEventListener('resize', this.updateDimensions);
     }
 
     updateDimensions() {
@@ -32,26 +29,7 @@ class Footer extends Component {
         });
     }
 
-    contactUs() {
-        const FooterDiv = styled.div`
-            float: right;
-            margin-right: 6%;
-            margin-top: 1%;
-        `;
-
-        if (window.innerWidth >= 541) {
-            return (
-                <FooterDiv>
-                    <Icon icon="contactUs" width="105px" />
-                </FooterDiv>
-            );
-        } else {
-            return null;
-        }
-    }
-
     render() {
-
         return (
             <footer className="page-footer">
                 <div className="footer-container">
@@ -59,25 +37,36 @@ class Footer extends Component {
                         <div className="col l4 s12">
                             <div className="row">
                                 <div className="col s12">
-                                    <img id="footer-logo" src={logo} alt="logo" />
+                                    <img
+                                        id="footer-logo"
+                                        src={logo}
+                                        alt="logo"
+                                    />
                                 </div>
                             </div>
                         </div>
                         <div className="col l4 s6">
-                            <p className="grey-text text-lighten-4 footer-item">Contact</p>
-                            <p className="grey-text text-lighten-4 footer-item">Support</p>
+                            <p className="grey-text text-lighten-4 footer-item">
+                                Contact
+                            </p>
+                            <p className="grey-text text-lighten-4 footer-item">
+                                Support
+                            </p>
                         </div>
                         <div className="col l4 s6">
-                            <p className="grey-text text-lighten-4 footer-item">Privacy</p>
-                            <p className="grey-text text-lighten-4 footer-item">Terms</p>
+                            <p className="grey-text text-lighten-4 footer-item">
+                                Privacy
+                            </p>
+                            <p className="grey-text text-lighten-4 footer-item">
+                                Terms
+                            </p>
                         </div>
-                        <div className="col offset-l4 l8 s12 footer-copyright"> © 2018 Laguro LLC. All Rights Reserved.
+                        <div className="col offset-l4 l8 s12 footer-copyright">
+                            {' '}
+                            © 2018 Laguro LLC. All Rights Reserved.
                         </div>
                     </div>
                 </div>
-
-                {this.contactUs()}
-
             </footer>
         );
     }
