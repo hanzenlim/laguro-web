78639cdda3affa90fe5243b633a296ddb3e46b1d
rrjoson <rrjoson08@gmail.com>
2018-06-18 16:50:29 +0800

    :sparkles: Implement progress bar

--- client/src/components/LoadingComponent.js
+++ client/src/components/LoadingComponent.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
-export default ({isLoading, error}) => {
+export default ({ isLoading, error }) => {
     // Handle the loading state
     if (isLoading) {
-        return <div></div>;
+        return <LinearProgress />;
     }
     // Handle the error state
     else if (error) {
         return <div>Sorry, there was a problem loading the page.</div>;
-    }
-    else {
+    } else {
         return null;
     }
 };
