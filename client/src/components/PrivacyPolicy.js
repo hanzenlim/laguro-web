a99ede1c5247945848e2111cbf59560f2fc9ffe8
Hanzen Lim <hanzen@laguro.com>
2018-06-23 14:58:51 -0700

    Add unique title to iframe for terms and privacy pages

--- client/src/components/PrivacyPolicy.js
+++ client/src/components/PrivacyPolicy.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
     <div>
-        <iframe width="100%" style={{height: '95vh'}} src="https://app.termly.io/document/privacy-policy/162a784d-109a-48d4-882f-c465efe41238" frameBorder="0" allowFullScreen>
+        <iframe title="Privacy" width="100%" style={{height: '95vh'}} src="https://app.termly.io/document/privacy-policy/162a784d-109a-48d4-882f-c465efe41238" frameBorder="0" allowFullScreen>
             <p>Your browser does not support iframes.</p>
         </iframe>
     </div>
