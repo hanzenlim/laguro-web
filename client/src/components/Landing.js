cf55d7581f6b84bbcfbb8f776b728d9012934b48
Jin Lee <jinlee8502@gmail.com>
2018-07-02 16:03:09 -0700

    implement dentist photogrid

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 import Search from "./Search";
 
 import HeroImg from './images/hero-img.png';
@@ -10,18 +12,34 @@ import Icon from './Icon';
 
 import './css/Landing.css'
 
-class Landing extends Component {
+const SearchTabH5 = styled.h5`
+    padding: 1.1rem 20px .66rem 20px;
+    margin: 0;
+    cursor: pointer;
+    font-size: 1.40rem;
+`;
+
+const IconWithMargin = styled(Icon)`
+    @media screen and (min-width : 541px) {
+        margin-top: 5%;
+    }
+`;
+
+export class LandingComponent extends Component {
     constructor() {
         super();
         this.state = {
             height: window.innerHeight,
-            width: window.innerWidth
+            width: window.innerWidth,
+            toggle: 'dentist'
         };
         this.updateDimensions = this.updateDimensions.bind(this);
     }
 
     componentDidMount() {
         window.addEventListener("resize", this.updateDimensions);
+        this.props.fetchOffices();
+        this.props.fetchActiveDentists();
     }
 
     componentWillUnmount() {
@@ -36,17 +54,9 @@ class Landing extends Component {
         });
     }
 
-    tabChange(event) {
-        const id = event.target.parentNode.id;
-        const element = document.getElementById(id);
-
-        const elementsToToggle = document.getElementsByClassName('toggle');
-
-        if (element && !element.classList.value.includes('active')) {
-            for (const el of elementsToToggle) {
-                el.classList.toggle('active');
-            }
-        }
+    tabChange = () => {
+        // console.log(event.currentTarget.id);
+        this.setState({toggle: this.state.toggle === 'dentist' ? 'office' : 'dentist'});
     }
 
     cta() {
@@ -61,77 +71,132 @@ class Landing extends Component {
         }
     }
 
-    render() {
-        const SearchTabH5 = styled.h5`
-            padding: 1.1rem 0 .66rem 0;
-            margin: 0;
-            cursor: pointer;
-            font-size: 1.40rem;
-        `;
-
-        const IconWithMargin = styled(Icon)`
-            @media screen and (min-width : 541px) {
-                margin-top: 5%;
-            }
-        `;
-
+    renderDentistVersion = () =>  {
         const mainIconWidth = (window.innerWidth >= 541)? "81px" : "45px";
-
+        const { dentists } = this.props;
         return (
             <div>
                 <div className="landing">
                     <div className="hero-search">
                         <IconWithMargin width={mainIconWidth} icon="logo" background="#FFFFFF" tooth="#0AD5B1" />
                         <div className="mission">
-                            <h3 className="toggle active headline"> No Waiting Rooms.</h3>
-                            <p className="toggle active explanation"> Find your next dentist or specialist and book an appointment in your neighborhood on your schedule.
+                            <h3 className="headline"> No Waiting Rooms.</h3>
+                            <p className="explanation"> Find your next dentist or specialist and book an appointment in your neighborhood on your schedule.
                             </p>
-                            <h3 className="toggle headline"> No Empty Chairs.</h3>
-                            <p className="toggle explanation"> Search, discover and book a chair after-hours at a local clinic and see your patients on your terms. </p>
                         </div>
                         <div className="search">
                             <div className="search-tabs">
                                 <div
-                                    className="tab active toggle"
+                                    className="tab"
                                     id="dentist-tab"
                                     onClick={this.tabChange}
                                 >
-                                    <SearchTabH5 className="search-tab">Find a Dentist</SearchTabH5>
+                                    <SearchTabH5 className="search-tab active">Find a Dentist</SearchTabH5>
                                 </div>
                                 <div
-                                    className="tab toggle"
+                                    className="tab inactive"
                                     id="office-tab"
                                     onClick={this.tabChange}
                                 >
                                     <SearchTabH5 className="search-tab">Rent an Office</SearchTabH5>
                                 </div>
                             </div>
-                            <div className="searchModule toggle active">
+                            <div className="searchModule">
                                 <Search search="dentist" />
                             </div>
-                            <div className="searchModule toggle">
+                        </div>
+                    </div>
+                </div>
+                <PhotoGrid data-name="dentist-photo-grid" numRow="2" page="landing" objects={dentists} header={"Popular Dentists"} />
+            </div>);
+    }
+
+    renderOfficeVersion = () =>  {
+        const mainIconWidth = (window.innerWidth >= 541)? "81px" : "45px";
+        const { offices } = this.props;
+        return (
+            <div>
+                <div className="landing">
+                    <div className="hero-search">
+                        <IconWithMargin width={mainIconWidth} icon="logo" background="#FFFFFF" tooth="#0AD5B1" />
+                        <div className="mission">
+                            <h3 className="headline"> No Empty Chairs.</h3>
+                            <p className="explanation"> Search, discover and book a chair after-hours at a local clinic and see your patients on your terms. </p>
+                        </div>
+                        <div className="search">
+                            <div className="search-tabs">
+                                <div
+                                    className="tab inactive"
+                                    id="dentist-tab"
+                                    onClick={this.tabChange}
+                                >
+                                    <SearchTabH5 className="search-tab">Find a Dentist</SearchTabH5>
+                                </div>
+                                <div
+                                    className="tab"
+                                    id="office-tab"
+                                    onClick={this.tabChange}
+                                >
+                                    <SearchTabH5 className="search-tab active">Rent an Office</SearchTabH5>
+                                </div>
+                            </div>
+                            <div className="searchModule">
                                 <Search search="office" />
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <PhotoGrid numRow="2" page="landing"/>
+                <PhotoGrid data-name="office-photo-grid" numRow="2" objects={offices} header={"New Listings"}/>
+            </div>);
+    }
 
+    render() {
+
+        return (
+            <div>
+                {this.state.toggle === 'dentist' && this.renderDentistVersion()}
+                {this.state.toggle === 'office' && this.renderOfficeVersion()}
                 <div id="cta-container">
                     {this.cta()}
                     <div id="cta-text">
                         <h2 className="cta-text-header"> Rent your Office </h2>
                         <h4 className="cta-text-blurb"> Have free space to share or have an open office? Rent your dental office with Laguro. </h4>
-
                         <Link id="get-started" to={'/landlord-onboarding/add-office'} className="login waves-effect btn white-text">Get Started</Link>
                     </div>
                 </div>
-
-
             </div>
         );
     }
 }
 
-export default Landing;
+function mapStateToProps(state) {
+    return {
+        offices: state.offices && state.offices.all && state.offices.all.map(office => {
+            const { id, name, location } = office;
+            return {
+                id,
+                name,
+                location,
+                //computing the average chairHourlyPrice and numChairsAvailable
+                imageUrl: office.imageUrls[0],
+                chairHourlyPrice: office.listings.map(listing => listing.chairHourlyPrice).reduce((a, b) => a + b) / office.listings.length,
+                numChairsAvailable: office.listings.map(listing => listing.numChairsAvailable).reduce((a, b) => a + b) / office.listings.length,
+                detailPageUrl: `/office/` + id
+            };
+        }),
+        dentists: state.dentists && state.dentists.dentists && state.dentists.dentists.map(dentist => {
+            const { id, procedures, location } = dentist;
+            return {
+                id,
+                location,
+                procedures,
+                imageUrl: dentist.user.imageUrl,
+                name: dentist.user.firstName + ' ' + dentist.user.lastName,
+                detailPageUrl: `/dentist/` + id
+            };
+        })
+    };
+}
+
+export default connect(mapStateToProps, actions)(LandingComponent);
