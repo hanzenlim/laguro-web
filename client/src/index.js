7f469d8ba220e5857871a51313aff1713508ec14
Hanzen Lim <hanzen@laguro.com>
2018-07-03 18:05:01 -0700

    Remove redux in prod

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,20 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(reduxThunk),
-);
+let store;
+
+if (process.env.NODE_ENV === 'production') {
+    store = createStore(
+        reducers,
+        applyMiddleware(reduxThunk),
+    );
+} else {
+    store = createStore(
+        reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        applyMiddleware(reduxThunk),
+    );
+}
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
