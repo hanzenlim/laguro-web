9037d3dd94303c110700e745f65164fb6fd39987
Jin Lee <jinlee8502@gmail.com>
2018-07-05 18:01:19 -0700

    implemented new design for edit office

--- client/src/components/forms/EditOffice.js
+++ client/src/components/forms/EditOffice.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { formValueSelector, Field, reduxForm, FieldArray } from 'redux-form';
-import ReactFilestack from 'filestack-react';
-import { Link } from 'react-router-dom';
-
-import { filestackKey } from '../../config/keys';
-import * as actions from '../../actions';
-import { renderPrice, removeSpecialChars } from '../../util/paymentUtil';
-import { required } from './formValidation';
 import {
-    renderField,
-    renderSelect,
-    equipmentOptions,
-    charCount
+    Field,
+    reduxForm,
+    formValueSelector,
+    FieldArray
+} from 'redux-form';
+import styled from 'styled-components';
+import ReactFilestack from 'filestack-react';
+import * as actions from '../../actions';
+import { filestackKey } from '../../config/keys';
+import { renderPrice, removeSpecialChars } from '../../util/paymentUtil';
+import { Typography, Grid, Button, Box } from '../common';
+import { Padding } from '../common/Spacing';
+import { renderField, charCount, renderSelect, equipmentOptions,
 } from './sharedComponents';
+import {
+    required,
+} from './formValidation';
+
+const StyledContainer = styled.div`
+    min-height: 100vh;
+    height: 100%;
+    max-width: 1080px;
+    padding: 5em 10px;
+    margin: 0 auto;
+`;
 
 class EditOffice extends Component {
     constructor(props) {
@@ -91,19 +103,19 @@ class EditOffice extends Component {
         });
     }
 
-    renderEquipmentSelector = ({ fields, className, meta: { error } }) => (
-        <ul className={className}>
+    renderEquipmentSelector = ({ fields, meta: { error } }) => (
+        <ul>
             <label>Equipment Available</label>
             {fields.map((equipment, index) => (
                 <li key={index} className="multiRowAdd">
                     <Field
                         name={`${equipment}.name`}
-                        label="Equipment Available"
+                        label="Type"
                         component={renderSelect}
                         validate={required}
                         children={equipmentOptions}
                     />
-                    <div className="col s2">
+                    <div>
                         <Field
                             name={`${equipment}.price`}
                             type="text"
@@ -126,120 +138,141 @@ class EditOffice extends Component {
                 </li>
             ))}
             <li>
-                <button
-                    type="button"
-                    className="waves-effect btn-flat"
-                    onClick={() => fields.push({})}
-                >
-                    Add Equipment
-                </button>
+                <Box ml={4}>
+                    <button
+                        type="button"
+                        className="waves-effect btn-flat"
+                        onClick={() => fields.push({})}
+                    >
+                        Add Equipment
+                    </button>
+                </Box>
                 {error && <span>{error}</span>}
             </li>
         </ul>
     );
 
     render() {
-        const { handleSubmit, submitting } = this.props;
+        const { handleSubmit, submitting, error } = this.props;
+
+        if (!this.props.initialized) {
+            return <div>Loading...</div>;
+        }
 
         return (
-            <form
-                className="bigForm light-blue lighten-5"
-                onSubmit={handleSubmit(this.onSubmit.bind(this))}
-            >
-                <div className="form_title">
-                    <h4>Edit Dentist Office</h4>
-                    <Link
-                        className="btn light-blue lighten-2 waves-effect"
-                        to={'/profile'}
-                    >
-                        Go back to profile
-                    </Link>
-                </div>
+            <StyledContainer>
+                <Grid container>
+                    <Grid item xs={12} sm={6}>
+                        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <Typography fontSize={5}>
+                                        Edit Dentist Office
+                                    </Typography>
+                                </Grid>
+                            </Grid>
 
-                <div className="row">
-                    <Field
-                        name="name"
-                        label="Name"
-                        className="col s12 m4"
-                        placeholder="Bell Dental"
-                        component={renderField}
-                        validate={required}
-                    />
-                </div>
+                            <Padding bottom="24" />
 
-                <Field
-                    name="description"
-                    label="Description (optional)"
-                    component={renderField}
-                    multiline={true}
-                    rows={2}
-                    charCount={true}
-                    inputProps={{
-                        maxLength: 500
-                    }}
-                />
+                            <Field
+                                name="name"
+                                label="Name"
+                                className="col s12"
+                                component={renderField}
+                                validate={required}
+                            />
 
-                {charCount(
-                    this.props.description ? this.props.description.length : 0,
-                    500
-                )}
+                            <Padding bottom="16" />
 
-                <div className="row">
-                    <FieldArray
-                        name="equipment"
-                        className="col s12"
-                        component={this.renderEquipmentSelector}
-                    />
-                </div>
+                            <Field
+                                name="description"
+                                label="Description (optional)"
+                                component={renderField}
+                                multiline={true}
+                                rows={2}
+                                charCount={true}
+                                inputProps={{
+                                    maxLength: 500
+                                }}
+                            />
 
-                <div className="image_upload">
-                    <div className="image_display">
-                        {this.renderUploadedImages()}
-                    </div>
-                    <ReactFilestack
-                        apikey={filestackKey}
-                        buttonText="Upload New Image"
-                        buttonClass="btn light-blue lighten-2"
-                        options={{
-                            accept: ['image/*'],
-                            imageMin: [300, 300],
-                            maxFiles: 5,
-                            fromSources: [
-                                'local_file_system',
-                                'url',
-                                'imagesearch',
-                                'facebook',
-                                'instagram'
-                            ],
-                            storeTo: { container: 'office-photos' }
-                        }}
-                        onSuccess={result => this.extractUrlToState(result)}
-                    />
-                </div>
+                            {charCount(
+                                this.props.description ? this.props.description.length : 0,
+                                500
+                            )}
 
-                <div className="form-buttons">
-                    <button
-                        className="waves-effect btn light-blue lighten-2"
-                        type="submit"
-                        disabled={submitting}
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
+                            <div className="row">
+                                <FieldArray
+                                    name="equipment"
+                                    className="col s12"
+                                    component={this.renderEquipmentSelector}
+                                />
+                            </div>
+
+                            <div className="image_upload">
+                                <div className="image_display">
+                                    {this.renderUploadedImages()}
+                                </div>
+                                <ReactFilestack
+                                    apikey={filestackKey}
+                                    buttonText="Upload New Image"
+                                    buttonClass="btn light-blue lighten-2"
+                                    options={{
+                                        accept: ['image/*'],
+                                        imageMin: [300, 300],
+                                        maxFiles: 5,
+                                        fromSources: [
+                                            'local_file_system',
+                                            'url',
+                                            'imagesearch',
+                                            'facebook',
+                                            'instagram'
+                                        ],
+                                        storeTo: { container: 'office-photos' }
+                                    }}
+                                    onSuccess={result => this.extractUrlToState(result)}
+                                />
+                            </div>
+
+                            <Box pb={4} />
+
+                            <Grid container justify="space-between">
+                                <Grid item>
+                                    {error && (
+                                        <strong className="red-text">
+                                            {error}
+                                        </strong>
+                                    )}
+                                    <Button
+                                        color="secondary"
+                                        type="submit"
+                                        disabled={submitting}
+                                    >
+                                        <Typography
+                                            fontSize={4}
+                                            fontWeight="medium"
+                                        >
+                                            Submit
+                                        </Typography>
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </Grid>
+                </Grid>
+            </StyledContainer>
         );
     }
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = state => {
     const selector = formValueSelector('editOffice');
-
     return {
         auth: state.auth,
         office: state.offices.selected,
         description: selector(state, 'description')
     };
-}
+};
 
 export default reduxForm({
     form: 'editOffice'
