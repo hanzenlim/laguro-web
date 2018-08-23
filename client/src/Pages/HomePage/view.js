2d4e08203c187960b2f2b92fa82d28e9c5991888
Jin Lee <jinlee8502@gmail.com>
2018-08-23 13:51:56 -0700

    addressing hanzen's comments

--- client/src/Pages/HomePage/view.js
+++ client/src/Pages/HomePage/view.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import hero1 from '../../legacyComponents/images/Hero-img.jpg';
 import { Box, Icon, Input, Text } from '../../components';
@@ -33,69 +33,62 @@ const StyledInput = styled(Input)`
     background-color: white;
 `;
 
-class HomePageView extends Component {
-    render() {
-        return (
-            <Box>
-                <Box
-                    background={String.raw`url(${hero1}) no-repeat right bottom 0/1920px auto`}
-                    backgroundSize="cover"
-                    height="100vh"
-                    width="100%"
-                >
-                    <Box
+const HomePageView = () => (
+    <Box>
+        <Box
+            data-name="hero-shot"
+            background={`url(${hero1}) no-repeat right bottom 0/1920px auto`}
+            backgroundSize="cover"
+            height="100vh"
+            width="100%"
+        >
+            <Box
+                color="text.white"
+                m="auto"
+                minWidth="840px"
+                pt="32vh"
+                width="45%"
+            >
+                <Box width="65%">
+                    <Text
+                        fontWeight="bold"
                         color="text.white"
-                        m="auto"
-                        minWidth="840px"
-                        pt="32vh"
-                        width="45%"
+                        fontSize={5}
+                        fontStyle="italic"
+                        lineHeight="50px"
                     >
-                        <Box width="65%">
-                            <Text
-                                fontWeight="bold"
-                                color="text.white"
-                                fontSize={5}
-                                fontStyle="italic"
-                                lineHeight="50px"
-                            >
-                                find your next
-                            </Text>
-                            <Text
-                                color="text.white"
-                                fontSize={6}
-                                fontStyle="italic"
-                                fontWeight="light"
-                                lineHeight="50px"
-                            >
-                                dentist or specialist
-                            </Text>
-                            <Text
-                                lineHeight="50px"
-                                color="text.white"
-                                fontWeight="light"
-                                fontStyle="italic"
-                                fontSize={5}
-                            >
-                                and book an appointment
-                            </Text>
-                        </Box>
-                        <Box pb={30} />
-                        <StyledInput
-                            placeholder="find your smile here"
-                            prefix={
-                                <Icon
-                                    type="locationPin"
-                                    width="40px"
-                                    height="40px"
-                                />
-                            }
-                        />
-                    </Box>
+                        find your next
+                    </Text>
+                    <Text
+                        color="text.white"
+                        fontSize={6}
+                        fontStyle="italic"
+                        fontWeight="light"
+                        lineHeight="50px"
+                    >
+                        dentist or specialist
+                    </Text>
+                    <Text
+                        lineHeight="50px"
+                        color="text.white"
+                        fontWeight="light"
+                        fontStyle="italic"
+                        fontSize={5}
+                    >
+                        and book an appointment
+                    </Text>
                 </Box>
-                <FeaturedList />
+                <Box pb={30} />
+                <StyledInput
+                    placeholder="find your smile here"
+                    prefix={
+                        <Icon type="locationPin" width="40px" height="40px" />
+                    }
+                />
             </Box>
-        );
-    }
-}
+        </Box>
+        <FeaturedList />
+    </Box>
+);
 
 export default HomePageView;
