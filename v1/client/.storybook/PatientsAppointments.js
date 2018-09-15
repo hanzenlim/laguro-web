8099dab45128e7a4a57eb62de5dc3df1ebf1c267
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2018-09-16 01:56:13 +0800

    Create PatientsAppointments component

---
+++ v1/client/.storybook/PatientsAppointments.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import PatientsAppointments from '../src/pages/common/PatientsAppointments';
+
+storiesOf('PatientsAppointments', module).add('default', () => (<PatientsAppointments />));
