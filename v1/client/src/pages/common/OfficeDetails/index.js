6888c47c0469627843a8ded19583ab768fab30a1
Samuel Cheng <sam@laguro.com>
2018-09-25 13:53:18 -0700

    Fix all errors associated with running pre-commit hook.

--- v1/client/src/pages/common/OfficeDetails/index.js
+++ v1/client/src/pages/common/OfficeDetails/index.js
@@ -30,13 +30,6 @@ class OfficeDetails extends PureComponent {
                     }
 
                     const office = data.getOffice;
-                    const {
-                        name,
-                        imageUrls,
-                        description,
-                        totalRating,
-                        numReviews,
-                    } = office;
 
                     const mappedData = {
                         name: office.name,
