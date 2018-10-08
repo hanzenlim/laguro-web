af75cb08f61db0fb5f7742fb0aa0a9e453f09d30
Samuel Cheng <sam@laguro.com>
2018-10-08 10:37:15 -0700

    Add comments

--- v1/client/src/pages/common/ReserveOffice/SelectReservation/index.js
+++ v1/client/src/pages/common/ReserveOffice/SelectReservation/index.js
@@ -788,6 +788,8 @@ class SelectReservation extends Component {
                                         selectedStartDate,
                                         selectedEndDate,
                                     ] = selectedDates;
+
+                                    // retrieve all listings within the selected date range
                                     const currentListings = data.queryListings.filter(
                                         listing => {
                                             const listingEnd = moment(
@@ -799,6 +801,10 @@ class SelectReservation extends Component {
 
                                             // the argument [] means isBetween includes
                                             // items at the boundary
+                                            // if selected start date is between the
+                                            // listing start date or the listing start date
+                                            // is between the selected interval, they intersect,
+                                            // so we include that listing
                                             return (
                                                 listingStart.isBetween(
                                                     selectedStartDate,
