9aa179c425c081fb1bc3ad49a1b84873cd2026b8
Ricardo Raphael <rrjoson08@gmail.com>
2018-11-16 01:58:09 +0800

    Fix homepage test (#703)

--- client/src/__tests__/pages/HomePage/view.test.js
+++ client/src/__tests__/pages/HomePage/view.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import HomePage from '../../../pages/HomePage';
+import HomePage from '../../../pages/HomePage/view';
 
 jest.mock('react-ga');
 
