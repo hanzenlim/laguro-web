38ccce6466ff3ce72f804250b73a1637a9be6475
Hanzen Lim <hanzen@laguro.com>
2018-11-16 13:42:19 -0800

    Fix cookie issue not logging out correctly

--- util/token.js
+++ util/token.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 
 const DEFAULT_EXPIRY = '7d';
 
+export const COOKIE_EXPIRATION = 86400000;
+
 export const generateToken = (user, expiresIn = DEFAULT_EXPIRY) => {
     const token = jwt.sign(
         {
