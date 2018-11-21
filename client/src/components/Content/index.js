c2fe58fd28654033db98e7279740c403bb6a998a
rrjoson <rrjoson08@gmail.com>
2018-11-22 02:36:00 +0800

    Async load header and homepage

--- client/src/components/Content/index.js
+++ client/src/components/Content/index.js
@@ -6,6 +6,8 @@ const StyledContent = styled(Box)`
     flex: 1;
     width: 100%;
     background: white;
+    min-height: 100vh;
+    height: 100%;
 `;
 
 const Content = props => {
--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 const StyledLayout = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    height: 100%;
+    min-height: 100vh;
     align-items: center;
 `;
 
