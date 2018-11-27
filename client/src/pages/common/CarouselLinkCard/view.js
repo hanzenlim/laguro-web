65bee251f87922f3faa470b62209e3d4000693d6
rrjoson <rrjoson08@gmail.com>
2018-11-28 01:57:57 +0800

    Fix non filestack image

--- client/src/pages/common/CarouselLinkCard/view.js
+++ client/src/pages/common/CarouselLinkCard/view.js
@@ -39,7 +39,7 @@ const CarouselLinkCard = props => {
                             width="100%"
                             pb={type === 'rectangle' ? '75%' : '100%'}
                         >
-                            {imageUrl ? (
+                            {imageUrl.includes('filestack') ? (
                                 <Box
                                     position="absolute"
                                     width="100%"
