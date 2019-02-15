ee9cca75c03b0e5fb68689e52227f459d2575e80
JOSELIE CASTANEDA <jcing.jc@gmail.com>
2019-02-15 08:50:11 +0800

    center login form, remove forgot password, change register now style

--- client/src/pages/common/Login/index.js
+++ client/src/pages/common/Login/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Alert } from 'antd';
 import { withRouter, Link } from 'react-router-dom';
 import _get from 'lodash/get';
+import styled from 'styled-components';
 
 import { Box, Flex, Responsive, Image, Text } from '../../../components';
 import { withScreenSizes } from '../../../components/Responsive';
@@ -10,74 +11,87 @@ import StandaloneLoginView from './view';
 
 const { TabletDesktop } = Responsive;
 
-const Login = ({
-    message,
-    location,
-    openForgotPassModal,
-    closeModal,
-    openRegistrationModal,
-    history,
-}) => {
-    return (
-        <Box height="100%">
-            <TabletDesktop>
-                <Flex flexDirection="row" height="100%">
-                    {message && <Alert message={message} type="info" />}
-                    <Box flex={1} borderRadius={4}>
-                        <Image
-                            src={loginModalImage}
-                            alt="Laguro login"
-                            width={443}
-                            height={664}
-                            borderRadius={4}
-                        />
-                    </Box>
-                    <Flex flex={1} p={20} mt={34} flexDirection="column">
-                        <Box width="100%" flex={9}>
-                            <StandaloneLoginView
-                                closeModal={closeModal}
-                                push={history.push}
+const StyledFlex = styled(Flex)`
+    && {
+        width: 100%;
+    }
+
+    && form {
+        width: 100%;
+    }
+`;
+
+class Login extends Component {
+    state = { showLoginTitle: true };
+
+    toggleLoginTitle = () => {
+        this.setState(state => ({
+            showLoginTitle: !state.showLoginTitle,
+        }));
+    };
+
+    render() {
+        const { message, location, closeModal, history } = this.props;
+        const { showLoginTitle } = this.state;
+
+        return (
+            <Box height="100%">
+                <TabletDesktop>
+                    <Flex flexDirection="row" height="100%">
+                        {message && <Alert message={message} type="info" />}
+                        <Box flex={1} borderRadius={4}>
+                            <Image
+                                src={loginModalImage}
+                                alt="Laguro login"
+                                width={443}
+                                height={664}
+                                borderRadius={4}
                             />
                         </Box>
-                        <Flex
-                            textAlign="center"
-                            justifyContent="center"
-                            mb={20}
-                            flex={1}
-                        >
-                            <Link
-                                to={{
-                                    hash: '#',
-                                    search: _get(location, 'search'),
-                                }}
-                                onClick={openRegistrationModal}
-                                width={140}
+                        <Flex flex={1} p={20} flexDirection="column">
+                            <StyledFlex
+                                flex={9}
+                                alignItems="center"
+                                justifyContent="center"
+                                px={10}
                             >
-                                <Text
-                                    color="text.blue"
-                                    textAlign="right"
-                                    fontWeight="bold"
+                                <StandaloneLoginView
+                                    closeModal={closeModal}
+                                    push={history.push}
+                                    title={
+                                        showLoginTitle
+                                            ? 'Please sign in'
+                                            : 'Sign up here'
+                                    }
+                                    authAction={
+                                        showLoginTitle ? 'sign in' : 'sign up'
+                                    }
+                                />
+                            </StyledFlex>
+                            <Flex
+                                textAlign="center"
+                                justifyContent="center"
+                                mb={20}
+                                flex={1}
+                            >
+                                <Link
+                                    to={{
+                                        hash: '#',
+                                        search: _get(location, 'search'),
+                                    }}
+                                    onClick={this.toggleLoginTitle}
+                                    width={140}
                                 >
-                                    register now
-                                </Text>
-                            </Link>
-                            <Text color="text.black">&nbsp;|&nbsp;</Text>
-                            <Link
-                                to={{
-                                    hash: '#',
-                                    search: _get(location, 'search'),
-                                }}
-                                onClick={openForgotPassModal}
-                            >
-                                <Text color="text.black" fontWeight="bold">
-                                    forgot password?
-                                </Text>
-                            </Link>
+                                    <Text color="text.blue" textAlign="right">
+                                        Register now
+                                    </Text>
+                                </Link>
+                            </Flex>
                         </Flex>
                     </Flex>
-                </Flex>
-            </TabletDesktop>
-        </Box>
-    );
-};
+                </TabletDesktop>
+            </Box>
+        );
+    }
+}
 export default withScreenSizes(withRouter(Login));
