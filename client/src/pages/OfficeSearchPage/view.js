64d4a8d8b3e5b6df57ecc2672acc3feb14330142
JOSELIE CASTANEDA <jcing.jc@gmail.com>
2019-03-13 16:36:50 +0800

    remove dummy data

--- client/src/pages/OfficeSearchPage/view.js
+++ client/src/pages/OfficeSearchPage/view.js
@@ -20,69 +20,6 @@ const Map = Loadable({
 
 const { Desktop } = Responsive;
 
-export const offices = [
-    {
-        id: 1,
-        title: 'Super Center',
-        image: null,
-        address: 'Super Place, 15, Waldorf, Maryland',
-
-        rating: 5,
-        numReviews: 13,
-        equipment: [
-            'Composites',
-            'Braces',
-            'X-rays',
-            'Lasers',
-            'Burs',
-            'Needles/Syringes',
-            'Needles/Syringes',
-        ],
-        subtitle:
-            'Super Center is a friendly family dental office with equipment and supplies available to facilitate implant procedures.',
-    },
-    {
-        id: 2,
-        title: 'Super Center',
-        image: null,
-        address: 'Super Place, 15, Waldorf, Maryland',
-
-        rating: 5,
-        numReviews: 13,
-        equipment: [
-            'Composites',
-            'Braces',
-            'X-rays',
-            'Lasers',
-            'Burs',
-            'Needles/Syringes',
-            'Needles/Syringes',
-        ],
-        subtitle:
-            'Super Center is a friendly family dental office with equipment and supplies available to facilitate implant procedures.',
-    },
-    {
-        id: 3,
-        title: 'Super Center',
-        image: 'https:bit.ly/laguro-tina',
-        address: 'Super Place, 15, Waldorf, Maryland',
-
-        rating: 5,
-        numReviews: 13,
-        equipment: [
-            'Composites',
-            'Braces',
-            'X-rays',
-            'Lasers',
-            'Burs',
-            'Needles/Syringes',
-            'Needles/Syringes',
-        ],
-        subtitle:
-            'Super Center is a friendly family dental office with equipment and supplies available to facilitate implant procedures.',
-    },
-];
-
 const OfficeSearchPageView = props => {
     const {
         data,
@@ -146,7 +83,7 @@ const OfficeSearchPageView = props => {
                         height={['auto', '', 'calc(100vh - 220px)']}
                     >
                         <SearchResultsList
-                            data={offices}
+                            data={data}
                             total={total}
                             showMap={showMap}
                             title="Office Results"
