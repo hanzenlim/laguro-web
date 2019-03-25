c3fb335e961e65856cc478b3c2e1e2ff323374c3
JOSELIE CASTANEDA <jcing.jc@gmail.com>
2019-03-26 00:46:23 +0800

    update backgound color of search pages

--- client/src/pages/DentistSearchPage/view.js
+++ client/src/pages/DentistSearchPage/view.js
@@ -82,88 +82,94 @@ const DentistSearchPageView = props => {
     });
 
     return (
-        <Container pt={[48, '', 160]} px={[0, 0, 25]}>
-            <Desktop>
-                {matches =>
-                    matches ? null : (
-                        <Box mb={20} mt={[24, '', 0]} px={[25, '', 0]}>
-                            <SearchBox
-                                size="large"
-                                placeholder="Search for dentists by name, location, or specialty"
-                            />
-                        </Box>
-                    )
-                }
-            </Desktop>
-            {total > 0 && (
-                <Flex justifyContent="space-between" mb="18px" px={[25, '', 0]}>
-                    <Text fontSize={['20px', '22px']} color="text.black">
-                        {urlParams.location && 'Dentists near '}
-                        <Text display="inline-block" fontWeight="bold">
-                            {urlParams.location}
-                        </Text>
-                        {urlParams.startTime && ' on '}
-                        {urlParams.startTime && (
-                            <Text display="inline-block" fontWeight="bold">
-                                {moment(urlParams.startTime).format(
-                                    'MMM D, YYYY'
-                                )}
-                            </Text>
-                        )}
-                    </Text>
-                    <Flex display={['none', '', 'flex']}>
-                        <Text fontSize="16px" color="text.black" mr="13px">
-                            Map View
-                        </Text>
-                        <Switch onClick={toggleMap} />
-                    </Flex>
-                </Flex>
-            )}
-            <Box mb={211}>
-                <Grid
-                    gridColumnGap={['', '', '33px']}
-                    gridTemplateColumns={[
-                        '1fr',
-                        '',
-                        `${total > 0 && showMap ? '1fr 1fr' : ''}`,
-                    ]}
-                >
-                    <Box>
-                        <Box
-                            style={{ overflow: 'scroll' }}
-                            height={['auto', '', 'calc(100vh - 220px)']}
-                        >
-                            <SearchResultsList
-                                data={data}
-                                total={total}
-                                showMap={showMap}
-                                onShowMore={onShowMore}
-                            />
-                        </Box>
-                    </Box>
-
-                    {total > 0 && showMap ? (
-                        <Desktop>
-                            <Box
-                                position="absolute"
-                                transform="translateX(calc(100% + 34px))"
-                                top="220px"
-                                height="calc(100vh - 220px)"
-                                bottom="0"
-                            >
-                                <Map
-                                    data={markers}
-                                    width={mapDimensions.width}
-                                    height={mapDimensions.height}
-                                    urlParams={urlParams}
-                                    defaultPosition={defaultPosition}
+        <Box bg="#f8f8f8">
+            <Container pt={[48, '', 160]} px={[0, 0, 25]}>
+                <Desktop>
+                    {matches =>
+                        matches ? null : (
+                            <Box mb={20} mt={[24, '', 0]} px={[25, '', 0]}>
+                                <SearchBox
+                                    size="large"
+                                    placeholder="Search for dentists by name, location, or specialty"
                                 />
                             </Box>
-                        </Desktop>
-                    ) : null}
-                </Grid>
-            </Box>
-        </Container>
+                        )
+                    }
+                </Desktop>
+                {total > 0 && (
+                    <Flex
+                        justifyContent="space-between"
+                        mb="18px"
+                        px={[25, '', 0]}
+                    >
+                        <Text fontSize={['20px', '22px']} color="text.black">
+                            {urlParams.location && 'Dentists near '}
+                            <Text display="inline-block" fontWeight="bold">
+                                {urlParams.location}
+                            </Text>
+                            {urlParams.startTime && ' on '}
+                            {urlParams.startTime && (
+                                <Text display="inline-block" fontWeight="bold">
+                                    {moment(urlParams.startTime).format(
+                                        'MMM D, YYYY'
+                                    )}
+                                </Text>
+                            )}
+                        </Text>
+                        <Flex display={['none', '', 'flex']}>
+                            <Text fontSize="16px" color="text.black" mr="13px">
+                                Map View
+                            </Text>
+                            <Switch onClick={toggleMap} />
+                        </Flex>
+                    </Flex>
+                )}
+                <Box mb={211}>
+                    <Grid
+                        gridColumnGap={['', '', '33px']}
+                        gridTemplateColumns={[
+                            '1fr',
+                            '',
+                            `${total > 0 && showMap ? '1fr 1fr' : ''}`,
+                        ]}
+                    >
+                        <Box>
+                            <Box
+                                style={{ overflow: 'scroll' }}
+                                height={['auto', '', 'calc(100vh - 220px)']}
+                            >
+                                <SearchResultsList
+                                    data={data}
+                                    total={total}
+                                    showMap={showMap}
+                                    onShowMore={onShowMore}
+                                />
+                            </Box>
+                        </Box>
+
+                        {total > 0 && showMap ? (
+                            <Desktop>
+                                <Box
+                                    position="absolute"
+                                    transform="translateX(calc(100% + 34px))"
+                                    top="220px"
+                                    height="calc(100vh - 220px)"
+                                    bottom="0"
+                                >
+                                    <Map
+                                        data={markers}
+                                        width={mapDimensions.width}
+                                        height={mapDimensions.height}
+                                        urlParams={urlParams}
+                                        defaultPosition={defaultPosition}
+                                    />
+                                </Box>
+                            </Desktop>
+                        ) : null}
+                    </Grid>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
--- client/src/pages/OfficeSearchPage/view.js
+++ client/src/pages/OfficeSearchPage/view.js
@@ -33,99 +33,104 @@ const OfficeSearchPageView = props => {
     } = props;
 
     return (
-        <Container pt={[48, '', 160]}>
-            <Desktop>
-                {matches =>
-                    matches ? null : (
-                        <Box mb={20} mt={[24, '', 0]}>
-                            <SearchBox
-                                size="large"
-                                placeholder="Search for dentists by name, location, or specialty"
-                            />
-                        </Box>
-                    )
-                }
-            </Desktop>
-            {total > 0 && (
-                <Flex justifyContent="space-between" mb="18px">
-                    {urlParams.text && !urlParams.location && (
-                        <Text fontSize={['20px', '22px']} color="text.black">
-                            Search results for{' '}
-                            <Text display="inline-block" fontWeight="bold">
-                                "{urlParams.text}"
-                            </Text>
-                        </Text>
-                    )}
-
-                    <Text fontSize={['20px', '22px']} color="text.black">
-                        {urlParams.location && 'Offices near '}
-                        <Text display="inline-block" fontWeight="bold">
-                            {urlParams.location}
-                        </Text>
-                        {urlParams.startTime && ' on '}
-                        {urlParams.startTime && (
-                            <Text display="inline-block" fontWeight="bold">
-                                {moment(urlParams.startTime).format(
-                                    'MMM D, YYYY'
-                                )}
-                            </Text>
-                        )}
-                    </Text>
-
-                    <Flex display={['none', '', 'flex']}>
-                        <Text fontSize="16px" color="text.black" mr="13px">
-                            Map View
-                        </Text>
-                        <Switch onClick={toggleMap} />
-                    </Flex>
-                </Flex>
-            )}
-            <Box mb={211}>
-                <Grid
-                    gridColumnGap={['', '', '33px']}
-                    gridTemplateColumns={[
-                        '1fr',
-                        '',
-                        `${total > 0 && showMap ? '1fr 1fr' : ''}`,
-                    ]}
-                >
-                    <Box>
-                        <Box
-                            style={{ overflow: 'scroll' }}
-                            height={['auto', '', 'calc(100vh - 220px)']}
-                        >
-                            <SearchResultsList
-                                data={data}
-                                total={total}
-                                showMap={showMap}
-                                title="Office Results"
-                                onShowMore={onShowMore}
-                            />
-                        </Box>
-                    </Box>
-
-                    {total > 0 && showMap ? (
-                        <Desktop>
-                            <Box
-                                position="absolute"
-                                transform="translateX(calc(100% + 34px))"
-                                top="220px"
-                                height="calc(100vh - 220px)"
-                                bottom="0"
-                            >
-                                <Map
-                                    data={data}
-                                    width={mapDimensions.width}
-                                    height={mapDimensions.height}
-                                    urlParams={urlParams}
-                                    defaultPosition={defaultPosition}
+        <Box bg="#f8f8f8">
+            <Container pt={[48, '', 160]}>
+                <Desktop>
+                    {matches =>
+                        matches ? null : (
+                            <Box mb={20} mt={[24, '', 0]}>
+                                <SearchBox
+                                    size="large"
+                                    placeholder="Search for dentists by name, location, or specialty"
                                 />
                             </Box>
-                        </Desktop>
-                    ) : null}
-                </Grid>
-            </Box>
-        </Container>
+                        )
+                    }
+                </Desktop>
+                {total > 0 && (
+                    <Flex justifyContent="space-between" mb="18px">
+                        {urlParams.text && !urlParams.location && (
+                            <Text
+                                fontSize={['20px', '22px']}
+                                color="text.black"
+                            >
+                                Search results for{' '}
+                                <Text display="inline-block" fontWeight="bold">
+                                    "{urlParams.text}"
+                                </Text>
+                            </Text>
+                        )}
+
+                        <Text fontSize={['20px', '22px']} color="text.black">
+                            {urlParams.location && 'Offices near '}
+                            <Text display="inline-block" fontWeight="bold">
+                                {urlParams.location}
+                            </Text>
+                            {urlParams.startTime && ' on '}
+                            {urlParams.startTime && (
+                                <Text display="inline-block" fontWeight="bold">
+                                    {moment(urlParams.startTime).format(
+                                        'MMM D, YYYY'
+                                    )}
+                                </Text>
+                            )}
+                        </Text>
+
+                        <Flex display={['none', '', 'flex']}>
+                            <Text fontSize="16px" color="text.black" mr="13px">
+                                Map View
+                            </Text>
+                            <Switch onClick={toggleMap} />
+                        </Flex>
+                    </Flex>
+                )}
+                <Box mb={211}>
+                    <Grid
+                        gridColumnGap={['', '', '33px']}
+                        gridTemplateColumns={[
+                            '1fr',
+                            '',
+                            `${total > 0 && showMap ? '1fr 1fr' : ''}`,
+                        ]}
+                    >
+                        <Box>
+                            <Box
+                                style={{ overflow: 'scroll' }}
+                                height={['auto', '', 'calc(100vh - 220px)']}
+                            >
+                                <SearchResultsList
+                                    data={data}
+                                    total={total}
+                                    showMap={showMap}
+                                    title="Office Results"
+                                    onShowMore={onShowMore}
+                                />
+                            </Box>
+                        </Box>
+
+                        {total > 0 && showMap ? (
+                            <Desktop>
+                                <Box
+                                    position="absolute"
+                                    transform="translateX(calc(100% + 34px))"
+                                    top="220px"
+                                    height="calc(100vh - 220px)"
+                                    bottom="0"
+                                >
+                                    <Map
+                                        data={data}
+                                        width={mapDimensions.width}
+                                        height={mapDimensions.height}
+                                        urlParams={urlParams}
+                                        defaultPosition={defaultPosition}
+                                    />
+                                </Box>
+                            </Desktop>
+                        ) : null}
+                    </Grid>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
