2e70ddda6987b91db04a1ea794658af901ef84d3
Ricardo Raphael <rrjoson08@gmail.com>
2019-03-28 00:02:27 +0800

    Remove old unit test (#1094)

--- client/src/__tests__/pages/HomePage/view.test.js
+++ client/src/__tests__/pages/HomePage/view.test.js
@@ -14,16 +14,4 @@ describe('HomePage', () => {
     afterEach(() => {
         component = '';
     });
-
-    it('should display text inside the top picture', () => {
-        const heroShotText = component.find('Text');
-
-        expect(heroShotText).toBeTruthy();
-        expect(
-            heroShotText
-                .at(0)
-                .childAt(0)
-                .text()
-        ).not.toEqual('');
-    });
 });
