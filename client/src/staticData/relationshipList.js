7df318118df7dc9bbea49eeb3c2473a6adef45c2
rrjoson <rrjoson08@gmail.com>
2019-06-22 08:22:59 +0800

    Update relationship list

--- client/src/staticData/relationshipList.js
+++ client/src/staticData/relationshipList.js
@@ -1,5 +1,9 @@
 export const relationshipList = [
     { key: 'CHILD', name: 'Child' },
     { key: 'SPOUSE', name: 'Spouse' },
+    { key: 'FRIEND', name: 'Friend' },
+    { key: 'SIBLING', name: 'Sibling' },
+    { key: 'PARENT', name: 'Parent' },
+    { key: 'RELATIVE', name: 'Relative' },
     { key: 'OTHER_DEPENDENT', name: 'Dependent' },
 ];
