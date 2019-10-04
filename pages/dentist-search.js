beb090b04f926a10b615fb33c032a7436e31be86
rrjoson <rrjoson08@gmail.com>
2019-10-04 13:05:38 +0800

    Remove slick carousel cdn import

--- pages/dentist-search.js
+++ pages/dentist-search.js
@@ -239,17 +239,6 @@ class DetailsSearchPage extends PureComponent {
                         rel="canonical"
                         href="https://www.laguro.com/dentist/search"
                     />
-                    <link
-                        rel="stylesheet"
-                        type="text/css"
-                        charSet="UTF-8"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-                    />
-                    <link
-                        rel="stylesheet"
-                        type="text/css"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-                    />
                 </Head>
                 <Query
                     query={GET_DENTISTS_AND_APPOINTMENT_SLOTS}
