d2f229609510197c692e2aa490c62d0b383cb413
RR Joson <rrjoson08@gmail.com>
2019-10-23 10:43:38 +0800

    Disable inline styles (#1637)

--- pages/_document.js
+++ pages/_document.js
@@ -5,29 +5,27 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 class InlineStylesHead extends Head {
-    getCssLinks() {
-        return this.__getInlineStyles();
-    }
-
-    __getInlineStyles() {
-        const { assetPrefix, files } = this.context._documentProps;
-        if (!files || files.length === 0) return null;
-
-        return files
-            .filter(file => /\.css$/.test(file))
-            .map(file => (
-                <style
-                    key={file}
-                    data-href={`${assetPrefix}/_next/${file}`}
-                    dangerouslySetInnerHTML={{
-                        __html: readFileSync(
-                            join(process.cwd(), '.next', file),
-                            'utf-8'
-                        ),
-                    }}
-                />
-            ));
-    }
+    // getCssLinks() {
+    //     return this.__getInlineStyles();
+    // }
+    // __getInlineStyles() {
+    //     const { assetPrefix, files } = this.context._documentProps;
+    //     if (!files || files.length === 0) return null;
+    //     return files
+    //         .filter(file => /\.css$/.test(file))
+    //         .map(file => (
+    //             <style
+    //                 key={file}
+    //                 data-href={`${assetPrefix}/_next/${file}`}
+    //                 dangerouslySetInnerHTML={{
+    //                     __html: readFileSync(
+    //                         join(process.cwd(), '.next', file),
+    //                         'utf-8'
+    //                     ),
+    //                 }}
+    //             />
+    //         ));
+    // }
 }
 
 export default class MyDocument extends Document {
